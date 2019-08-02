6712df488e3003e2741509145593f00b4500c351
Shaun <srugh@xtra.co.nz>
2019-08-03 09:38:59 +1200

    Added player forward movement

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = new Vector3(0, 5, 0);
+    }
+}
