c96f90b3487fcfed7e27d36d18fcbd9185806932
Malachi <malachi.t.bartley@gmail.com>
2019-08-03 21:21:43 +1200

    Player Improvements, completed level 2 and begun work on level 3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,7 @@
+using Assets.Scripts;
+using System;
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,6 +14,11 @@ public class Player : MonoBehaviour
     public Quaternion StartRotation;
 
     private int impulseTimer = 0;
+
+
+    private float moveTimer = 0.5f;
+    private bool canMove = true;
+    private bool moveCounting = false;
 
     // Start is called before the first frame update
     void Start()
@@ -48,13 +57,50 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnCollisionExit(Collision collision)
+    {
+        if (Constants.FloorTags.Contains(collision.gameObject.tag))
+        {
+            StartCoroutine(StopMovingAfterSeconds(moveTimer));
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Constants.FloorTags.Contains(collision.gameObject.tag))
+        {
+            if (moveCounting)
+            {
+                StopCoroutine(StopMovingAfterSeconds(moveTimer));
+            }
+            if (!canMove)
+            {
+                print("Can start moving again");
+                canMove = true;
+            }
+        }
+    }
+
+    IEnumerator StopMovingAfterSeconds(float seconds)
+    {
+        moveCounting = true;
+        yield return new WaitForSeconds(seconds);
+        canMove = false;
+        moveCounting = false;
+    }
+
     void FixedUpdate()
     {
+        if (!canMove)
+        {
+            print("Cant Move");
+            return;
+        }
+
         if (impulseTimer > 0)
         {
-            impulseTimer--;
+            impulseTimer -= 1/50;
             DashLoaded();
-
         }
 
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
@@ -68,7 +114,7 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.Space) && impulseTimer == 0)
         {
             rigidBody.AddRelativeForce(new Vector3(0, 0, 55), ForceMode.Impulse);
-            impulseTimer = 2 * 50;
+            impulseTimer = 2;
         
         }
     }
