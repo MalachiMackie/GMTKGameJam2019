70cf2209a2cd1e24eac404e87b54a99cae2192b4
Malachi <malachi.t.bartley@gmail.com>
2019-08-04 15:49:52 +1200

    Progress on Level 3

--- Assets/Scripts/MovingBlock.cs
+++ Assets/Scripts/MovingBlock.cs
@@ -1,35 +1,70 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class MovingBlock : MonoBehaviour
 {
-    public Vector3 startPos;
-
-    public Vector3 endPos;
-
-    public float travelTime = 1;
+    public float TravelTime = 1;
 
     private float travelSpeed;
 
     private Vector3 nextPos;
 
+    public float timeOffset = 0;
+
+    private bool Activated;
+
+    public bool Debug;
+
+    public List<Vector3> TravelPoints = new List<Vector3>();
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = startPos;
-        nextPos = endPos;
-
-        var distance = (startPos - endPos).magnitude;
-        travelSpeed = distance / (50 / (1/(float)travelTime));
+        transform.position = TravelPoints[0];
+        nextPos = TravelPoints[1];
+        
+        travelSpeed = CalculateSpeed(transform.position, nextPos, TravelTime);
+        StartCoroutine(Utils.DoAfterSeconds(() => { Activated = true; }, timeOffset));
     }
 
     // FixedUpdate is called 50 times per second
     void FixedUpdate()
     {
-        if ((transform.position - nextPos).magnitude <= 0)
+        if (Activated)
         {
-            nextPos = nextPos == startPos ? endPos : startPos;
+            if (Debug)
+            {
+                print(transform.position);
+                print(nextPos);
+                print((transform.position - nextPos).magnitude);
+            }
+            if ((transform.position - nextPos).magnitude <= 0)
+            {
+                NextPoint();
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, nextPos, travelSpeed);
+        }
+    }
+
+    private void NextPoint()
+    {
+        var currentPos = nextPos;
+        var index = TravelPoints.IndexOf(nextPos) + 1;
+        if (index == TravelPoints.Count)
+        {
+            index = 0;
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, nextPos, travelSpeed);
+        nextPos = TravelPoints[index];
+
+        travelSpeed = CalculateSpeed(currentPos, nextPos, TravelTime);
+    }
+
+    public float CalculateSpeed(Vector3 pos1, Vector3 pos2, float travelTime)
+    {
+        var distance = (pos1 - pos2).magnitude;
+        return distance / (50 / (1 / travelTime));
     }
 }
